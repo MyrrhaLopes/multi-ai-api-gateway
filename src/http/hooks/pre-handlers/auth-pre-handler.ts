70b5fb5c0f0ad81da4af9381ac51708a75de422d
Lorenzo Myrrha <lorenzolopes223@gmail.com>
2026-03-07 16:37:03 -0300

    started db lookup for authenticating api key

--- src/http/hooks/pre-handlers/auth-pre-handler.ts
+++ src/http/hooks/pre-handlers/auth-pre-handler.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify"
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import z from "zod"
 import { AuthService } from "../../../core/services/auth-service.ts"
+import { ClientError } from "../../globals/errors/client-error.ts"
 
 const authHeaderSchema = z.object({
   'API Key': z.string({error: "API Key is required"}).min(32,{error:"Invalid API KEY"})
@@ -17,11 +18,13 @@ export const authPreHandler : FastifyPluginAsyncZod = async (app){
     }
     const apiKey = result.data["API Key"]
 
+    //TODO: adicionar isso a entity de Auth
+    class AuthError extends ClientError{}
     //autentica se o usuário está presente no sistema
     const authService = new AuthService();
     const { user, error } = authService.authenticate(apiKey);
     if (!user) {
-      return ;
+      throw new AuthError("API key not present");
     }
   }
 }
